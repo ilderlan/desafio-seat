de25e058bf56077aca26df1d65ca042a73d6fdc3
ilderlan.pinho <ilderlan.pinho@DIEAD-SEPVI-003.grupont.com.br>
2017-09-28 15:15:05 -0300

    Função para ler do usuário a posição do número primo.

--- desafio-seaf/src/problema7/Primos.java
+++ desafio-seaf/src/problema7/Primos.java
@@ -1,11 +1,29 @@
 package problema7;
 
+import java.util.Scanner;
+
 public class Primos {
 
 	public static void main(String[] args) {
 		int contadorDePrimos = 0;
 		int primoFinal = 10001;
 		
+		Scanner leitor = new Scanner(System.in);
+		
+		
+		try{
+			do{
+				if(primoFinal <= 0)
+					System.out.println("Digite a posição no número primo que deseja encontrar. (OBS.: Dessa vez o númeor precisa ser positivo ;)");
+				else
+					System.out.println("Digite a posição no número primo que deseja encontrar.");
+				primoFinal = leitor.nextInt();
+			
+			}while(primoFinal <= 0);
+		}catch (Exception e) {
+			System.out.println("Entrada com valor inválido.");
+			return;
+		}
 		for (int primo = 2; contadorDePrimos <= primoFinal; primo++) {
 			int cont = 0;
 			
@@ -19,7 +37,7 @@ public class Primos {
 			if(cont <= 1){
 				++contadorDePrimos;
 				if(contadorDePrimos == primoFinal){
-					System.out.println("Posição: " + contadorDePrimos+"° = " + "Esse e o primo que eu quero: " + primo);
+					System.out.println("Posição: " + contadorDePrimos+"° = " + "Esse e o número primo desta posição: " + primo);
 					break;
 				}else
 					System.out.println("Posição " + contadorDePrimos+"° = " + primo);
